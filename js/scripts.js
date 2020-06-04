3d1af36e6c8ab7875ce8fccb09741893a0b3b9ad
keidsvik <61095627+keidsvik@users.noreply.github.com>
2020-06-04 13:19:31 -0700

    create initial html document and fill out skeleton.
    Co-authored by: Peter Grimm <pagrimm@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+
+
+
+
+//Equilateral: All sides are equal.
+//Isosceles: Exactly 2 sides are equal.
+//Scalene: No sides are equal.
+//NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.
